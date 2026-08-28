c305032b92846e16e06342683573e63316de324b
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 14:13:03 +0300

    STYLE: apply canonical publish route formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 VideoHub::Engine.routes.draw do
-  get "/feed" => "videos#index", defaults: { format: :json }
-  post "/" => "videos#create", defaults: { format: :json }
+  get "/feed" => "videos#index", :defaults => { format: :json }
+  post "/" => "videos#create", :defaults => { format: :json }
 end
