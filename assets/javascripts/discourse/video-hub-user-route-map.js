daddd22cafe201f478cac3dc5184b8c9eb68dab1
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 21:49:15 +0300

    FEATURE: register profile layout editor route

--- assets/javascripts/discourse/video-hub-user-route-map.js
+++ assets/javascripts/discourse/video-hub-user-route-map.js
@@ -2,5 +2,6 @@ export default {
   resource: "user",
   map() {
     this.route("video-hub-profile", { path: "/videos" });
+    this.route("video-hub-profile-layout", { path: "/videos/edit" });
   },
 };
