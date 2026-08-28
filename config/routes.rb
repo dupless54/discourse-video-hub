0114c827d2fa4db9f72e85f06bc4e236652f7f3a
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 14:55:34 +0300

    FEATURE: route JSON watch reads

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@
 
 VideoHub::Engine.routes.draw do
   get "/feed" => "videos#index", :defaults => { format: :json }
+  get "/:id/:slug" => "videos#show",
+      :constraints => {
+        id: /\d+/,
+        format: :json,
+      }
   post "/" => "videos#create", :defaults => { format: :json }
 end
