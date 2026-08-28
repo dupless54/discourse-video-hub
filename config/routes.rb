7f9794e00715789fde2487a4294f4248d7ebe679
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 21:18:43 +0300

    FEATURE: route authorized profile layout reads

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 VideoHub::Engine.routes.draw do
   get "/feed" => "videos#index", :defaults => { format: :json }
   get "/profile/:username" => "profiles#show", :defaults => { format: :json }
+  get "/profile/:username/layout" => "profiles#layout", :defaults => { format: :json }
   put "/profile/:username/layout" => "profiles#update_layout", :defaults => { format: :json }
   get "/:id/:slug" => "videos#show", :constraints => { id: /\d+/, format: :json }
   post "/" => "videos#create", :defaults => { format: :json }
