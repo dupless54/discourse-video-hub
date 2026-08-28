2a6e473ce5ca708266ddfd7f61c0fa2ee15626a5
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 22:27:11 +0300

    STYLE: format profile membership routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,19 @@ VideoHub::Engine.routes.draw do
   get "/profile/:username/layout" => "profiles#layout", :defaults => { format: :json }
   put "/profile/:username/layout" => "profiles#update_layout", :defaults => { format: :json }
   put "/profile/:username/layout/videos/:video_id" => "profiles#add_layout_video",
-      :constraints => { video_id: /\d+/ },
-      :defaults => { format: :json }
+      :constraints => {
+        video_id: /\d+/,
+      },
+      :defaults => {
+        format: :json,
+      }
   delete "/profile/:username/layout/videos/:video_id" => "profiles#remove_layout_video",
-         :constraints => { video_id: /\d+/ },
-         :defaults => { format: :json }
+         :constraints => {
+           video_id: /\d+/,
+         },
+         :defaults => {
+           format: :json,
+         }
   get "/:id/:slug" => "videos#show", :constraints => { id: /\d+/, format: :json }
   post "/" => "videos#create", :defaults => { format: :json }
 end
