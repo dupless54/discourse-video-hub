4a3f7631fdf8dca48b76931c62d3d964f4e9d049
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-28 14:59:50 +0300

    STYLE: apply canonical watch route formatting

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@
 
 VideoHub::Engine.routes.draw do
   get "/feed" => "videos#index", :defaults => { format: :json }
-  get "/:id/:slug" => "videos#show",
-      :constraints => {
-        id: /\d+/,
-        format: :json,
-      }
+  get "/:id/:slug" => "videos#show", :constraints => { id: /\d+/, format: :json }
   post "/" => "videos#create", :defaults => { format: :json }
 end
