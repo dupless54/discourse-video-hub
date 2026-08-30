21a54a950ab81a7f02c741608e1b72e09ff40127
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-30 22:44:52 +0300

    STYLE: apply canonical SyntaxTree route formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-VideoHub::Engine.routes.draw { get "/feed" => "videos#index", defaults: { format: :json } }
+VideoHub::Engine.routes.draw do
+  get "/feed" => "videos#index", defaults: { format: :json }
+end
