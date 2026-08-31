afe738abb829288958c50f7ca0295a6cda93d2a7
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 11:04:28 +0300

    FEATURE: register public collection route

--- assets/javascripts/discourse/video-hub-route-map.js
+++ assets/javascripts/discourse/video-hub-route-map.js
@@ -4,5 +4,6 @@ export default function () {
   this.route("video-hub-trending", { path: "/videos/trending" });
   this.route("video-hub-following", { path: "/videos/following" });
   this.route("video-hub-saved", { path: "/videos/saved" });
+  this.route("video-hub-collection", { path: "/videos/collections/:id" });
   this.route("video-hub-watch", { path: "/videos/:id/:slug" });
 }
