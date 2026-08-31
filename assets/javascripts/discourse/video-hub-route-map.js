4e4d9620a71e1bf1b5db61f80b5996f5056ce199
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 08:47:06 +0300

    FEATURE: route following videos page

--- assets/javascripts/discourse/video-hub-route-map.js
+++ assets/javascripts/discourse/video-hub-route-map.js
@@ -2,6 +2,7 @@ export default function () {
   this.route("videos", { path: "/videos" });
   this.route("video-hub-new", { path: "/videos/new" });
   this.route("video-hub-trending", { path: "/videos/trending" });
+  this.route("video-hub-following", { path: "/videos/following" });
   this.route("video-hub-saved", { path: "/videos/saved" });
   this.route("video-hub-watch", { path: "/videos/:id/:slug" });
 }
