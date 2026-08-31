de91d72a524724003d1747c2e5e005e463e625d0
dupless54 <126967564+dupless54@users.noreply.github.com>
2026-08-31 06:49:45 +0300

    FEATURE: register trending videos route

--- assets/javascripts/discourse/video-hub-route-map.js
+++ assets/javascripts/discourse/video-hub-route-map.js
@@ -1,6 +1,7 @@
 export default function () {
   this.route("videos", { path: "/videos" });
   this.route("video-hub-new", { path: "/videos/new" });
+  this.route("video-hub-trending", { path: "/videos/trending" });
   this.route("video-hub-saved", { path: "/videos/saved" });
   this.route("video-hub-watch", { path: "/videos/:id/:slug" });
 }
